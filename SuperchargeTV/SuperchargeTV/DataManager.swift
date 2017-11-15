642b0a32e84522521d61d066cdf429ff6ce1c529
brenor2 <rbrenorios@gmail.com>
2017-11-14 21:35:31 -0300

    Version working fine. I put this in another branch to keep separate from what I did within the 3 hour deadline

---
+++ SuperchargeTV/SuperchargeTV/DataManager.swift
@@ -0,0 +1,13 @@
+//
+//  DataManager.swift
+//  SuperchargeTV
+//
+//  Created by Breno Ramos on 14/11/17.
+//  Copyright © 2017 brenor2. All rights reserved.
+//
+
+import UIKit
+
+class DataManager: NSObject {
+
+}
